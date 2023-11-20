e08996d46dc3c21978b7ffd9b5d99cac12d21b52
Gudleif <edbgua@mac.com>
2023-11-20 20:49:15 +0100

    Initial Commit

---
+++ GitHubHalloTromsø/GitHubHalloTroms_App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubHalloTroms_App.swift
+//  GitHubHalloTromsø
+//
+//  Created by Gudleif Aronsen on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubHalloTroms_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
